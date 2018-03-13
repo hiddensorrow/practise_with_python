fc3ce53ac4dce676ea58e64378f9b844c386eb19
mayyesterday <cindyborn@163.com>
2018-03-13 18:56:11 +0800

    add result

--- day01/day01.py
+++ day01/day01.py
@@ -10,3 +10,4 @@ fontsize = min(xSize, ySize) // 12
 myFont = ImageFont.truetype('HYXiaRiTiW.ttf', fontsize)
 drawAvatar.text([0.9*xSize, 0.1*ySize-fontsize], text='1', fill='red', font=myFont)
 im.show()
+im.save('result.JPEG')
