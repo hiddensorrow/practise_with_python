a18753ca58d38b2b008cf68b1010c7fa6c00b7c8
mayyesterday <cindyborn@163.com>
2018-03-14 10:53:06 +0800

    [update]使用string.ascii_letters

--- day02/day02.py
+++ day02/day02.py
@@ -1,10 +1,12 @@
 # 生成200个激活码
 # 2018年03月14日10:43:54
 import random
+import string
 
-upper_alphabet = [chr(ord('A') + i) for i in range(26)]
-lower_alphabet = [chr(ord('a') + i) for i in range(26)]
-candidate = upper_alphabet + lower_alphabet
+# upper_alphabet = [chr(ord('A') + i) for i in range(26)]
+# lower_alphabet = [chr(ord('a') + i) for i in range(26)]
+# candidate = upper_alphabet + lower_alphabet
+candidate = string.ascii_letters
 # print(upper_alphabet)
 # print(lower_alphabet)
 # print(candidate)
