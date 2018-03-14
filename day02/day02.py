b67d18ba5adf9f22bae7a0ed0d467ea45b0930c5
mayyesterday <cindyborn@163.com>
2018-03-14 10:46:58 +0800

    [update]update day02

---
+++ day02/day02.py
@@ -0,0 +1,23 @@
+# 生成200个激活码
+# 2018年03月14日10:43:54
+import random
+
+upper_alphabet = [chr(ord('A') + i) for i in range(26)]
+lower_alphabet = [chr(ord('a') + i) for i in range(26)]
+candidate = upper_alphabet + lower_alphabet
+# print(upper_alphabet)
+# print(lower_alphabet)
+# print(candidate)
+ans, times = dict(), 0
+with open('res.txt', 'w') as f:
+    while times < 200:
+        res = ''
+        for i in range(20):
+          res += random.choice(candidate)
+        if res not in ans:
+            ans[res] = 1
+            times += 1
+            f.write(res+'\n')
+# print(len(ans))
+# for key, value in ans.items():
+#     print(key)
