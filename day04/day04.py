4391ac8615b5cd66fafaceef84209e7e0e48be8b
mayyesterday <cindyborn@163.com>
2018-03-15 16:35:15 +0800

    采用pipeline处理

--- day04/day04.py
+++ day04/day04.py
@@ -5,11 +5,12 @@ import string
 import redis
 
 
-red = redis.Redis(host='localhost', port=6379)
+red = redis.Redis(host='localhost', port=6379, db=1)
 candidate = string.ascii_letters
 
 ans, times = dict(), 0
 
+p = red.pipeline()
 while times < 200:
     res = ''
     for i in range(20):
@@ -17,4 +18,5 @@ while times < 200:
     if res not in ans:
         ans[res] = 1
         times += 1
-        red.set(times, res)
+        p.set(times, res)
+p.execute()
